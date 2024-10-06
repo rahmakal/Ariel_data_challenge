3a4c0fa42bb15bb25917796c820faf28f62561f7
Rahma <rahma.kallel.89@gmail.com>
2024-10-06 15:37:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import torch
 if __name__ == '__main__':
 
     train_dataloader, val_dataloader = split_data(
-        airs_path=r"C:\Users\SKYMIL\Desktop\vscode\kaggle\ariel_exoplanets\data\airs_v4.npy",
-        fgs_path=r"C:\Users\SKYMIL\Desktop\vscode\kaggle\ariel_exoplanets\data\fgs_v4.npy",
-        labels_path=r"C:\Users\SKYMIL\Desktop\vscode\kaggle\ariel_exoplanets\data\train_labels.csv"
+        airs_path="airs_v4.npy",
+        fgs_path="fgs_v4.npy",
+        labels_path="train_labels.csv"
     )
     # batch = Shape of x: torch.Size([32, 600]), Shape of y: torch.Size([32, 283])
     input_dims = 600
